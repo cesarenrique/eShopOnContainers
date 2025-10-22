ec830fb44036d6ef8fcbed4e0eb1dd89b0eafed6
Cesar Enrique Pozo Vasquez <cesarpozo25@gmail.com>
2025-10-22 19:33:28 +0200

    carpetas

--- src/Services/Catalog.API/Controllers/CatalogBrandsController.cs
+++ src/Services/Catalog.API/Controllers/CatalogBrandsController.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Catalog.API.Data;
 using Catalog.API.Model;
+using Catalog.API.Data;
 
 namespace Catalog.API.Controllers
 {
